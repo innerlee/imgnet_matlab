ac4f4691937b83535373da1d763912adfe07ff60
lizz <lizz@pku.edu.cn>
2015-08-07 10:30:24 +0800

    init co.

---
+++ LoadFeatures.m
@@ -0,0 +1,24 @@
+function [ features ] = LoadFeatures( path )
+%LoadFeatures Load imagenet fetures from .txt files in a folder.
+%
+%  Inputs:
+%   path: the folder contains a list of .txt files.
+%     Filenames are wnids.
+%     Each row is a vector of an image. 
+%     Use space as delimators.
+%
+%  Returns:
+%    features: a list of cells with properties
+%       .name: wnid, ie, filenames.
+%       .data: features vectors in colums. 
+%                 Note this is different from files.
+%       
+% Author: lizz
+% Date: 2015/08/07
+%
+
+
+
+
+end
+
