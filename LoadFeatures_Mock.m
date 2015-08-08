51674259622ee37201ed7ea08350ffbd374d382e
lizz <lizz@pku.edu.cn>
2015-08-08 14:14:03 +0800

    mock

---
+++ LoadFeatures_Mock.m
@@ -0,0 +1,32 @@
+function [ features ] = LoadFeatures_Mock( )
+%LoadFeatures Load imagenet fetures from .txt files in a folder.
+%
+%  Inputs:
+%   path: the folder contains a list of .txt files.
+%     Filenames are wnids.
+%     Each row is a vector of an image. 
+%     Use space as delimators.
+%
+%  Returns:
+%    features: a list of cells with properties
+%       .name: wnid, ie, filenames.
+%       .data: features vectors in rows. 
+%       
+% Author: lizz
+% Date: 2015/08/07
+%
+
+featurelength=1000;
+N=1200;
+totsets=100;
+
+features(totsets).wnids='';
+features(totsets).data=[];
+for i=1:totsets
+    features(i).wnids=['test' int2str(i)];
+    features(i).data=randi([1,3])* rand(N,featurelength);
+end
+
+
+end
+
